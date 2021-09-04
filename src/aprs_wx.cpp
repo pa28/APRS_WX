a00b885a51d7e3c632d46067da07123ab023a6dd
Richard Buckley <richard.buckley@ieee.org>
2021-09-04 17:52:36 -0400

    Add try catch block around main to catch and report exceptions.

--- src/aprs_wx.cpp
+++ src/aprs_wx.cpp
@@ -104,175 +104,173 @@ int main(int argc, char **argv) {
                      {"influxDb", ConfigItem::InfluxDb}
              }};
 
-    xdg::Environment &environment{xdg::Environment::getEnvironment()};
-    filesystem::path configFilePath = environment.appResourcesAppend("config.txt");
+    try {
+        xdg::Environment &environment{xdg::Environment::getEnvironment(true)};
+        filesystem::path configFilePath{"/etc/APRS_WX/config.txt"};
 
-    InputParser inputParser{argc, argv};
-    std::optional<std::string> callsign{};
-    std::optional<std::string> passCode{};
-    std::string filter{};
-    std::optional<double> qthLatitude{};
-    std::optional<double> qthLongitude{};
-    std::optional<long> filterRadius{};
+        InputParser inputParser{argc, argv};
+        std::optional<std::string> callsign{};
+        std::optional<std::string> passCode{};
+        std::string filter{};
+        std::optional<double> qthLatitude{};
+        std::optional<double> qthLongitude{};
+        std::optional<long> filterRadius{};
 
-    bool influxTls{false};
-    std::optional<std::string> influxHost{};
-    std::optional<long> influxPort{};
-    std::optional<std::string> influxDb{};
+        bool influxTls{false};
+        std::optional<std::string> influxHost{};
+        std::optional<long> influxPort{};
+        std::optional<std::string> influxDb{};
 
-    std::signal(SIGINT, signalHandler);
-    std::signal(SIGTERM, signalHandler);
-    std::signal(SIGHUP, signalHandler);
+        std::signal(SIGINT, signalHandler);
+        std::signal(SIGTERM, signalHandler);
+        std::signal(SIGHUP, signalHandler);
 
-    WeatherAggregator weatherAggregator{};
+        WeatherAggregator weatherAggregator{};
 
-    if (inputParser.cmdOptionExists(ConfigOption))
-        configFilePath = std::filesystem::path{inputParser.getCmdOption(ConfigOption)};
+        if (inputParser.cmdOptionExists(ConfigOption))
+            configFilePath = std::filesystem::path{inputParser.getCmdOption(ConfigOption)};
 
-    ConfigFile configFile{configFilePath};
-    if (auto status = configFile.open(); status == ConfigFile::OK) {
-        bool validFile{true};
-        configFile.process(ConfigSpec, [&](std::size_t idx, const std::string_view &data)
-        {
-            // ToDo: Implement item type decoding.
-            bool validValue{false};
-            switch (static_cast<ConfigItem>(idx)) {
-                case ConfigItem::Callsign:
-                    callsign = ConfigFile::parseText(data, [](char c) {
-                        return ConfigFile::isalnum(c) || c == '-';
-                    }, ConfigFile::toupper);
-                    validValue = callsign.has_value();
-                    break;
-                case ConfigItem::Passcode:
-                    passCode = ConfigFile::parseText(data, ConfigFile::isdigit);
-                    validValue = passCode.has_value();
-                    break;
-                    break;
-                case ConfigItem::Latitude:
-                    qthLatitude = configFile.safeConvert<double>(data);
-                    validValue = qthLatitude.has_value();
-                    break;
-                case ConfigItem::Longitude:
-                    qthLongitude = configFile.safeConvert<double>(data);
-                    validValue = qthLongitude.has_value();
-                    break;
-                case ConfigItem::Radius:
-                    filterRadius = configFile.safeConvert<long>(data);
-                    validValue = filterRadius.has_value();
-                    break;
-                case ConfigItem::InfluxTLS: {
-                    std::optional<long> value = configFile.safeConvert<long>(data);
-                    validValue = value.has_value();
-                    if (validValue)
-                        influxTls = value.value() != 0;
+        ConfigFile configFile{configFilePath};
+        if (auto status = configFile.open(); status == ConfigFile::OK) {
+            bool validFile{true};
+            configFile.process(ConfigSpec, [&](std::size_t idx, const std::string_view &data) {
+                // ToDo: Implement item type decoding.
+                bool validValue{false};
+                switch (static_cast<ConfigItem>(idx)) {
+                    case ConfigItem::Callsign:
+                        callsign = ConfigFile::parseText(data, [](char c) {
+                            return ConfigFile::isalnum(c) || c == '-';
+                        }, ConfigFile::toupper);
+                        validValue = callsign.has_value();
+                        break;
+                    case ConfigItem::Passcode:
+                        passCode = ConfigFile::parseText(data, ConfigFile::isdigit);
+                        validValue = passCode.has_value();
+                        break;
+                        break;
+                    case ConfigItem::Latitude:
+                        qthLatitude = configFile.safeConvert<double>(data);
+                        validValue = qthLatitude.has_value();
+                        break;
+                    case ConfigItem::Longitude:
+                        qthLongitude = configFile.safeConvert<double>(data);
+                        validValue = qthLongitude.has_value();
+                        break;
+                    case ConfigItem::Radius:
+                        filterRadius = configFile.safeConvert<long>(data);
+                        validValue = filterRadius.has_value();
+                        break;
+                    case ConfigItem::InfluxTLS: {
+                        std::optional<long> value = configFile.safeConvert<long>(data);
+                        validValue = value.has_value();
+                        if (validValue)
+                            influxTls = value.value() != 0;
+                    }
+                        break;
+                    case ConfigItem::InfluxHost:
+                        influxHost = ConfigFile::parseText(data, [](char c) {
+                            return ConfigFile::isalnum(c) || c == '.';
+                        });
+                        validValue = influxHost.has_value();
+                        break;
+                    case ConfigItem::InfluxPort:
+                        influxPort = configFile.safeConvert<long>(data);
+                        validValue = influxPort.has_value() && influxPort.value() > 0;
+                        break;
+                    case ConfigItem::InfluxDb:
+                        influxDb = ConfigFile::parseText(data, [](char c) {
+                            return ConfigFile::isalnum(c) || c == '_';
+                        });
+                        validValue = influxDb.has_value();
+                        break;
                 }
-                    break;
-                case ConfigItem::InfluxHost:
-                    influxHost = ConfigFile::parseText(data, [](char c) {
-                        return ConfigFile::isalnum(c) || c == '.';
-                    });
-                    validValue = influxHost.has_value();
-                    break;
-                case ConfigItem::InfluxPort:
-                    influxPort = configFile.safeConvert<long>(data);
-                    validValue = influxPort.has_value() && influxPort.value() > 0;
-                    break;
-                case ConfigItem::InfluxDb:
-                    influxDb = ConfigFile::parseText(data, [](char c) {
-                        return ConfigFile::isalnum(c) || c == '_';
-                    });
-                    validValue = influxDb.has_value();
-                    break;
+                validFile = validFile & validValue;
+                if (!validValue) {
+                    std::cerr << "Invalid config value: " << ConfigSpec[idx].mKey << '\n';
+                }
+            });
+            configFile.close();
+            if (!validFile) {
+                std::cerr << "Invalid configuration file " << configFilePath << " exiting.\n";
             }
-            validFile = validFile & validValue;
-            if (!validValue) {
-                std::cerr << "Invalid config value: " << ConfigSpec[idx].mKey << '\n';
-            }
-        });
-        configFile.close();
-        if (!validFile) {
-            std::cerr << "Invalid configuration file " << configFilePath << " exiting.\n";
+
+            if (!(influxHost.has_value() && influxPort.has_value() && influxDb.has_value()))
+                std::cerr << "Influx database not specified in " << configFilePath << ", data will not be stored.\n";
+        } else if (status == ConfigFile::NO_FILE) {
+            cerr << "Configuration file specified " << configFilePath << " does not exist.\n";
+            exit(1);
+        } else if (status == ConfigFile::OPEN_FAIL) {
+            cerr << "Could not open configuration file " << configFilePath << ": "
+                 << std::strerror(errno) << '\n';
+            exit(1);
         }
 
-        if (!(influxHost.has_value() && influxPort.has_value() && influxDb.has_value()))
-            std::cerr << "Influx database not specified in " << configFilePath << ", data will not be stored.\n";
-    } else if (status == ConfigFile::NO_FILE) {
-        cerr << "Configuration file specified " << configFilePath << " does not exist.\n";
-        exit(1);
-    } else if (status == ConfigFile::OPEN_FAIL) {
-        cerr << "Could not open configuration file " << configFilePath << ": " <<
+        if (callsign.has_value() && callsign.value().rfind("N0CALL") == 0) {
+            cerr << "Configuration file " << configFilePath << " not configured.\n";
+            exit(1);
+        }
 
-             std::strerror(errno)
+        std::stringstream filterStrm{};
+        filterStrm << "r/" << qthLatitude.value()
+                   << '/' << qthLongitude.value()
+                   << '/' << filterRadius.value();
 
-             << '\n';
-        exit(1);
-    }
+        filter = filterStrm.str();
 
-    std::stringstream filterStrm{};
-    filterStrm << "r/" << qthLatitude.
+        std::cerr << "Hello, CWOP APRS-IS!" << '\n'
+                  << callsign.value()
+                  << ' ' << filter << '\n';
 
-            value()
+        APRS_IS sock{callsign.value(), passCode.value(), filter};
+        sock.mQthPosition.
+                mLat = qthLatitude;
+        sock.mQthPosition.
+                mLon = qthLongitude;
+        sock.
+                mRadius = filterRadius;
 
-               << '/' << qthLongitude.
+        if (sock.
 
-            value()
+                openConnection()
 
-               << '/' << filterRadius.
+                ) {
+            while (run) {
+                sock.
 
-            value();
+                        getPacket();
 
-    filter = filterStrm.str();
+                if (!sock.mPacket.
 
-    std::cerr << "Hello, CWOP APRS-IS!" << '\n'
-              << callsign.value()
-              << ' ' << filter << '\n';
+                        empty()
 
-    APRS_IS sock{callsign.value(), passCode.value(), filter};
-    sock.mQthPosition.
-            mLat = qthLatitude;
-    sock.mQthPosition.
-            mLon = qthLongitude;
-    sock.
-            mRadius = filterRadius;
-
-    if (sock.
-
-            openConnection()
-
-            ) {
-        while (run) {
-            sock.
-
-                    getPacket();
-
-            if (!sock.mPacket.
-
-                    empty()
-
-                    ) {
-                if (!sock.prefix("# aprsc")) {
-                    if (sock.charAtIndex() != '#') {
-                        auto packet = sock.decode();
-                        switch (packet->status()) {
-                            case PacketStatus::WxPacket: {
-                                auto wx = std::unique_ptr<APRS_WX_Report>(
-                                        dynamic_cast<APRS_WX_Report *>(packet.release()));
-                                weatherAggregator[wx->mName] = std::move(wx);
-                                weatherAggregator.aggregateData();
-                                if (influxHost.has_value() && influxPort.has_value() && influxDb.has_value())
-                                    weatherAggregator.pushToInflux(influxHost.value(), influxTls, influxPort.value(), influxDb.value());
+                        ) {
+                    if (!sock.prefix("# aprsc")) {
+                        if (sock.charAtIndex() != '#') {
+                            auto packet = sock.decode();
+                            switch (packet->status()) {
+                                case PacketStatus::WxPacket: {
+                                    auto wx = std::unique_ptr<APRS_WX_Report>(
+                                            dynamic_cast<APRS_WX_Report *>(packet.release()));
+                                    weatherAggregator[wx->mName] = std::move(wx);
+                                    weatherAggregator.aggregateData();
+                                    if (influxHost.has_value() && influxPort.has_value() && influxDb.has_value())
+                                        weatherAggregator.pushToInflux(influxHost.value(), influxTls,
+                                                                       influxPort.value(), influxDb.value());
+                                }
+                                    break;
+                                default:
+                                    break;
                             }
-                                break;
-                            default:
-                                break;
                         }
                     }
                 }
             }
         }
-    }
 
-    sock.close();
+        sock.close();
+    } catch (exception& e) {
+        cerr << e.what() << '\n';
+    }
 
     return 0;
 }
