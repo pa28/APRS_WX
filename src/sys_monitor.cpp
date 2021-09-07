1d72fe1a9cfbdb635d58eb3280ea32a689eb709e
Richard Buckley <richard.buckley@ieee.org>
2021-09-07 14:33:28 -0400

    Add CPU usage reporting.

--- src/sys_monitor.cpp
+++ src/sys_monitor.cpp
@@ -8,6 +8,7 @@
 #include <filesystem>
 #include <csignal>
 #include <atomic>
+#include <system_error>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
@@ -30,6 +31,84 @@ void signalHandler(int signum) {
 
     run = false;
 }
+
+class CpuStats {
+protected:
+    enum Item {
+        User,
+        Nice,
+        System,
+        Idle,
+        IOWait,
+        IRQ,
+        SoftIRQ,
+        Steal,
+        Guest,
+        GuestNice,
+        ItemCount   // Must be last.
+    };
+
+    std::string mName{};
+    static constexpr std::string_view mProcStatFile {"/proc/stat"};
+    std::filesystem::path mProcStatPath{};
+    std::array<long, static_cast<std::size_t>(ItemCount)> mData{};
+
+public:
+    CpuStats() {
+        mProcStatPath = mProcStatFile;
+        for (auto &item : mData) {
+            item = 0;
+        }
+    }
+
+    bool getData() {
+        std::ifstream ifs;
+        ifs.open(mProcStatPath, std::ofstream::in);
+        if (ifs) {
+            std::string line{};
+            std::getline(ifs, line);
+            ifs.close();
+            auto line_view = std::string_view{line};
+            auto p = line_view.find(' ');
+            mName = line_view.substr(0, p);
+            p = line_view.find_first_of("0123456789", p);
+            auto idx = static_cast<std::size_t>(User);
+            while (p != std::string_view::npos && idx < static_cast<std::size_t>(ItemCount)) {
+                long value{};
+                auto [ptr, ec] {std::from_chars(line_view.begin()+p, line_view.end(), value)};
+                if (ec == std::errc()) {
+                    mData[static_cast<std::size_t>(idx)] = value - mData[static_cast<std::size_t>(idx)];
+                    p = line_view.find_first_of("0123456789", ptr - line_view.data());
+                    ++idx;
+                } else if (ec == std::errc::invalid_argument) {
+                    std::cerr << "CPU Stat invalid argument\n";
+                    return false;
+                } else if (ec == std::errc::result_out_of_range) {
+                    std::cerr << "CPU Stat result out of range\n";
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    std::tuple<long,long> getUsage() const {
+        long used{}, idle{};
+        for (auto idx = static_cast<std::size_t>(User); idx < static_cast<std::size_t>(ItemCount); ++idx) {
+            switch (static_cast<Item>(idx)) {
+                case Idle:
+                case IOWait:
+                    idle += mData[static_cast<std::size_t>(idx)];
+                    break;
+                default:
+                    used += mData[static_cast<std::size_t>(idx)];
+            }
+        }
+
+        return std::make_tuple(used, idle);
+    }
+};
 
 int main(int argc, char **argv) {
     static constexpr std::string_view ConfigOption = "--config";
@@ -75,6 +154,7 @@ int main(int argc, char **argv) {
         std::optional<long> influxPort{};
         std::optional<std::string> influxDb{};
         filesystem::path cpuZone{};
+        CpuStats cpuStats{};
 
         // Catch signals
         std::signal(SIGINT, signalHandler);
@@ -159,6 +239,12 @@ int main(int argc, char **argv) {
                         ifs.close();
                         measurements << prefix.str() << "cpuTemp=" << temperature / 1000 << '\n';
                     }
+                }
+
+                if (cpuStats.getData()) {
+                    auto [used, idle] = cpuStats.getUsage();
+                    double percentUsage = 100.0 * static_cast<double>(used) / static_cast<double>(used + idle);
+                    measurements << prefix.str() << "cpuUse=" << percentUsage << '\n';
                 }
 
                 auto data = measurements.str();
